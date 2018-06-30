8a158e5385ee47d796d0b40da9e7a98a2d44a6cf
elkrot <elkrot@gmail.com>
2018-06-30 14:34:46 +0300

    Init commit

---
+++ Game.py
@@ -0,0 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.join(sys.path[0], '../'))
+from Game import *
+
+MakhnoVersus().start()
---
+++ Game/Shared/GameConstants.py
@@ -0,0 +1,13 @@
+import os
+
+class GameConstants:
+    SCREEN_SIZE = [800, 600]
+    SPRITE_BALL = os.path.join("Assets", "ball.png")
+    SPRITE_HIGHSCORE = os.path.join("Game", "Assets", "highscore.png")
+    SPRITE_MENU = os.path.join("Game", "Assets", "menu.png")
+    SOUND_FILE_GAMEOVER = os.path.join("Game", "Assets", "GameOver.wav")
+    SOUND_FILE_GAMEOVER = 
+    PLAYING_SCENE = 0
+    GAMEOVER_SCENE = 1
+    HIGHSCORE_SCENE = 2
+    MENU_SCENE = 3
